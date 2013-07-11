53766ab0ea4ddb01a9deb66d1d74898b8c552a0f
Kumaran <kumaran.sivanathan12@gmail.com>
2013-07-10 23:58:51 -0400

    updated code and deleted unneeded files

--- rpc.cpp
+++ rpc.cpp
@@ -26,6 +26,8 @@ struct sockaddr_in my_addr;
 socklen_t sl =  sizeof(my_addr);
 socklen_t addrlen;
 
+vector<func> database;
+
 fd_set master;    // master file descriptor list
 fd_set read_fds;  // temp file descriptor list for select()
 int fdmax;        // maximum file descriptor number
@@ -53,6 +55,10 @@ int rpcInit(){
 	struct sockaddr_in sa;
 	struct hostent *hp;
 	int yes=1;
+	int sockfd, numbytes;
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	char s[INET6_ADDRSTRLEN];
 
 
 	//////////////////////////
@@ -114,46 +120,178 @@ int rpcInit(){
 	char* binder_port = getenv("BINDER_PORT");
 	cout << "Binder Port is" << binder_port << endl;
 
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
 
-	return(listener);
-}
-
-
-int rpcCall(char* name, int* argTypes, void** args){
-	
-	struct sockaddr_in sa;
-	struct hostent *hp;
-	int yes=1;	
-
-	bool successful = false;
-	bool warning = false;
-	bool error = false;
-
-/*
-		the client calls the rpcCall function
-		connects to the binder		
-*/
-
-
-	if (successful){
-		return 0;
-	}else if (warning){
+	if ((rv = getaddrinfo(binder_address, binder_port, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "Can't Get Address info for Binder: %s\n", gai_strerror(rv));
 		return 1;
-	} else if (error){
+	}
+
+	// loop through all the results and connect to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("Server: socket");
+			continue;
+		}
+
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("Server: connect");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "Server: failed to connect\n");
 		return -1;
 	}
 
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+			s, sizeof s);
+	printf("Server: connecting to %s\n", s);
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	FD_SET(sockfd, &master);
+
+	return 0;
 }
 
+int rpcCall(char* name, int* argTypes, void** args){
+
+}
 
 int rpcCacheCall(char* name, int* argTypes, void** args){
+
 }
 
 int rpcRegister(char* name, int* argTypes, skeleton f){
+	//Add to database
+
+	func new_func;
+	new_func.signature.name = name;
+	new_func.signature.argTypes = argTypes;
+	new_func.f = f;
+
+	database.insert(new_func);
+
+	//Calculate Length of Message
+	int hostname = sizeof(char) * (strlen(myname)) + 1;
+	int port = listening_port;
+	int len_name = sizeof(char) * (strlen(name)) + 1;
+	int argnum = calc_argnum(argTypes);
+	int arg_length = sizeof(int) * (argnum);
+
+	int content_length = len_name + hostname + port + arg_length;
+
+	rpc_message message;
+	message.type = REGISTER;
+	message.length = content_length;
+
+
+	write(binderfd, &message, sizeof(message));
+	char header[1000];
+
+	cout<<"Name is "<<name<<" and num params is "<<arg_length<<endl;
+
+	char *sendbuf = header;
+	memcpy(sendbuf, &hostname, sizeof(int));
+	sendbuf+=sizeof(int);
+	memcpy(sendbuf, myname, hostname);
+	sendbuf+=hostname;
+	memcpy(sendbuf, &port, sizeof(int));
+	sendbuf+=sizeof(int);
+	memcpy(sendbuf, &len_name, sizeof(int));
+	sendbuf+=sizeof(int);
+	memcpy(sendbuf, name, len_name);
+	sendbuf+=len_name;
+	memcpy(sendbuf, &arg_length, sizeof(int));
+	sendbuf+=sizeof(int);
+	memcpy(sendbuf, argTypes, arg_length);
+	sendbuf+=sizeof(arg_length);
+
+	send(binderfd, header, 1000, 0);
+
+	// Done Registering
 
 }
 
 int rpcExecute(){
+	   FD_SET(listener, &master);
+	   struct sockaddr_storage remoteaddr; // client address
+	   char remoteIP[INET6_ADDRSTRLEN];
+	   int newfd, i;
+	   int terminate = 0;
+	    // keep track of the biggest file descriptor
+	    fdmax = listener; // so far, it's this one
+	    for(;terminate == 0;) {
+	        read_fds = master; // copy it
+	        if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
+	            perror("select");
+	            exit(4);
+	        }
+
+	        // run through the existing connections looking for data to read
+	        for(i = 0; i <= fdmax; i++) {
+	            if (FD_ISSET(i, &read_fds)) { // we got one!!
+	                if (i == listener) {
+	                    // handle new connections
+	                    addrlen = sizeof remoteaddr;
+	                    newfd = accept(listener,
+	                        (struct sockaddr *)&remoteaddr,
+	                        &addrlen);
+
+	                    if (newfd == -1) {
+	                        perror("accept");
+	                    } else {
+	                        FD_SET(newfd, &master); // add to master set
+	                        if (newfd > fdmax) {    // keep track of the max
+	                            fdmax = newfd;
+	                        }
+	                        printf("selectserver: new connection from %s on "
+	                            "socket %d\n",
+	                            inet_ntop(remoteaddr.ss_family,
+	                                get_in_addr((struct sockaddr*)&remoteaddr),
+	                                remoteIP, INET6_ADDRSTRLEN),
+	                            newfd);
+	                    }
+	                }
+
+			else if(i == binderfd) {
+				cout<<"Message Received from Binder"<<endl;
+				rpc_message message;
+				read(binderfd, &message, sizeof(message));
+				if(message.type == TERMINATE) {
+					terminate = 1;
+					break;
+				}
+			}
+			else {
+	                     	cout<<"Server: Received Data From Someone"<<endl;
+				pthread_t t;
+				ThreadData* td = (ThreadData*) malloc(sizeof(ThreadData));
+
+	   			td->id = i;
+				pthread_create(&t, NULL, &parse_message,td);
+
+				FD_CLR(i, &master);
+				thread_pool.push_back(t);
+
+	                } // END handle data from client
+	            } // END got new incoming connection
+	        } // END looping through file descriptors
+	    } // END for(;;)--and you thought it would never end!
+
+
+		for(i = 0; i < thread_pool.size(); i++) {
+			pthread_join(thread_pool[i], NULL);
+		}
+		cout<<"Server is shutting down. Bye!"<<endl;
 
 }
 
@@ -161,7 +299,4 @@ int rpcTerminate(){
 
 }
 
-int main(){
-
-
-}
+int main(){}
